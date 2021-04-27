c4111ecace74a300357cd1708c40c50ef7ed47a2
tommywwz <wangweizhe2001@gmail.com>
2021-04-27 21:11:31 +0200

    move files from lab1 to lab2

--- ece3849_lab2_wwang8/main.c
+++ ece3849_lab2_wwang8/main.c
@@ -14,9 +14,20 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
 #include "driverlib/interrupt.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pwm.h"
+#include "driverlib/pin_map.h"
+
+#include "buttons.h"
+#include "sampling.h"
 
 uint32_t gSystemClock = 120000000; // [Hz] system clock frequency
+
+void signal_init(void);
 
 /*
  *  ======== main ========
@@ -24,6 +35,9 @@ uint32_t gSystemClock = 120000000; // [Hz] system clock frequency
 int main(void)
 {
     IntMasterDisable();
+
+    // initialize PWM
+    signal_init();
 
     // hardware initialization goes here
 
@@ -40,4 +54,24 @@ void task0_func(UArg arg1, UArg arg2)
     while (true) {
         // do nothing
     }
+}
+
+void signal_init(void) {
+    // configure M0PWM2, at GPIO PF2, BoosterPack 1 header C1 pin 2
+    // configure M0PWM3, at GPIO PF3, BoosterPack 1 header C1 pin 3
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_2 | GPIO_PIN_3);
+    GPIOPinConfigure(GPIO_PF2_M0PWM2);
+    GPIOPinConfigure(GPIO_PF3_M0PWM3);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_2 | GPIO_PIN_3,
+                     GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD);
+    // configure the PWM0 peripheral, gen 1, outputs 2 and 3
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
+    PWMClockSet(PWM0_BASE, PWM_SYSCLK_DIV_1); // use system clock without division
+    PWMGenConfigure(PWM0_BASE, PWM_GEN_1, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1, roundf((float)gSystemClock/PWM_FREQUENCY));
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, roundf((float)gSystemClock/PWM_FREQUENCY*0.4f));
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3, roundf((float)gSystemClock/PWM_FREQUENCY*0.4f));
+    PWMOutputState(PWM0_BASE, PWM_OUT_2_BIT | PWM_OUT_3_BIT, true);
+    PWMGenEnable(PWM0_BASE, PWM_GEN_1);
 }
