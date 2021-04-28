60fa674413467bcd75258e01d153506aafd598f0
tommywwz <wangweizhe2001@gmail.com>
2021-04-28 19:47:27 +0200

    backup button task [main]

--- ece3849_lab2_wwang8/main.c
+++ ece3849_lab2_wwang8/main.c
@@ -22,6 +22,7 @@
 #include "driverlib/pwm.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
 #include "Crystalfontz128x128_ST7735.h"
 #include "inc/tm4c1294ncpdt.h"
 
@@ -45,10 +46,17 @@ int RisingTrigger(void);
 int FallingTrigger(void);
 void loadBuffer(uint16_t* locBuffer, int trigger);
 
+void waveform_task(UArg arg1, UArg arg2);
+void processing_task(UArg arg1, UArg arg2);
+void display_task(UArg arg1, UArg arg2);
+void button_clock(void);
+void button_task(UArg arg1, UArg arg2);
+
 //debug
 uint32_t task1cnt = 0;
 uint32_t task2cnt = 0;
 uint32_t task3cnt = 0;
+uint32_t clkcnt = 0;
 
 /*
  *  ======== main ========
@@ -108,6 +116,9 @@ void processing_task(UArg arg1, UArg arg2) // lowest priority
 void display_task(UArg arg1, UArg arg2) // low priority
 {
     int x, y,nxt_x,nxt_y;
+    char button = 0;
+    char button_old, pressed;
+    bool trigger_mod = 1;
 
     Crystalfontz128x128_Init(); // Initialize the LCD display driver
     Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP); // set screen orientation
@@ -118,8 +129,15 @@ void display_task(UArg arg1, UArg arg2) // low priority
 
     while(true) {
         Semaphore_pend(screenupdate, BIOS_WAIT_FOREVER); // pending on trigger from processing task
+        task3cnt ++; // debug
+        button_old = button;
 
-        task3cnt ++;
+        Mailbox_pend(button_mailbox, &button, BIOS_WAIT_FOREVER);
+
+        pressed = ~button & button_old; // detect the button from pressed to non pressed
+
+        if (pressed & 1)
+            trigger_mod = !trigger_mod; // if sw 1 pressed, flip the trigger mode
 
         // draw grid
         GrContextForegroundSet(&sContext, ClrDarkBlue);
@@ -143,7 +161,55 @@ void display_task(UArg arg1, UArg arg2) // low priority
             x++;
         }
 
+        if (trigger_mod) {
+            // draw rising trigger shape
+            GrLineDrawH(&sContext, LCD_HORIZONTAL_MAX-20, LCD_HORIZONTAL_MAX-15, 6);
+            GrLineDrawH(&sContext, LCD_HORIZONTAL_MAX-15, LCD_HORIZONTAL_MAX-10, 0);
+            GrLineDrawV(&sContext, LCD_HORIZONTAL_MAX-15, 6, 0);
+            // draw rising trigger arrow
+            GrLineDraw (&sContext, LCD_HORIZONTAL_MAX-17, 3, LCD_HORIZONTAL_MAX-15, 1);
+            GrLineDraw (&sContext, LCD_HORIZONTAL_MAX-13, 3, LCD_HORIZONTAL_MAX-15, 1);
+        } else {
+            // draw falling trigger shape
+            GrLineDrawH(&sContext, LCD_HORIZONTAL_MAX-15, LCD_HORIZONTAL_MAX-10, 6);
+            GrLineDrawH(&sContext, LCD_HORIZONTAL_MAX-20, LCD_HORIZONTAL_MAX-15, 0);
+            GrLineDrawV(&sContext, LCD_HORIZONTAL_MAX-15, 6, 0);
+            // draw falling trigger arrow
+            GrLineDraw (&sContext, LCD_HORIZONTAL_MAX-17, 2, LCD_HORIZONTAL_MAX-15, 4);
+            GrLineDraw (&sContext, LCD_HORIZONTAL_MAX-13, 2, LCD_HORIZONTAL_MAX-15, 4);
+        }
+
         GrFlush(&sContext); // flush the frame buffer to the LCD
+    }
+}
+
+void button_clock(void) // clock function
+{
+    clkcnt++;
+    Semaphore_post(sample_btn);
+}
+
+
+void button_task(UArg arg1, UArg arg2) // high priority
+{
+    char loc_button;
+    while(true) {
+        Semaphore_pend(sample_btn, BIOS_WAIT_FOREVER);
+
+        // read hardware button state
+        uint32_t gpio_buttons = ~GPIOPinRead(GPIO_PORTJ_BASE, 0xff) & (GPIO_PIN_1 | GPIO_PIN_0); // EK-TM4C1294XL buttons in positions 0 and 1
+        gpio_buttons = gpio_buttons | ((~GPIOPinRead(GPIO_PORTH_BASE, 0xff) & GPIO_PIN_1) << 1); // load S1 to bitmap
+        gpio_buttons = gpio_buttons | ((~GPIOPinRead(GPIO_PORTK_BASE, 0xff) & GPIO_PIN_6) >> 3); // load S2 to bitmap
+        gpio_buttons = gpio_buttons | (~GPIOPinRead(GPIO_PORTD_BASE, 0xff) & GPIO_PIN_4);        // load Joystick select to bitmap
+
+        uint32_t old_buttons = gButtons;    // save previous button state
+        ButtonDebounce(gpio_buttons);       // Run the button debouncer. The result is in gButtons.
+        ButtonReadJoystick();               // Convert joystick state to button presses. The result is in gButtons.
+        uint32_t presses = ~old_buttons & gButtons;   // detect button presses (transitions from not pressed to pressed)
+        presses |= ButtonAutoRepeat();      // autorepeat presses if a button is held long enough
+
+        loc_button = (char)gButtons;
+        Mailbox_post(button_mailbox, &loc_button, BIOS_WAIT_FOREVER);
     }
 }
 
@@ -166,23 +232,23 @@ int RisingTrigger(void) // search for rising edge trigger
     return x;
 }
 
-//int FallingTrigger(void) // search for falling edge trigger
-//{
-//    // Step 1
-//    int32_t locBufferIndex = gADCBufferIndex;
-//    int x = locBufferIndex - LCD_HORIZONTAL_MAX/2/* half screen width; don’t use a magic number */;
-//    // Step 2
-//    int x_stop = x - ADC_BUFFER_SIZE/2;
-//    for (; x > x_stop; x--) {
-//        if (gADCBuffer[ADC_BUFFER_WRAP(x)] <= ADC_OFFSET &&
-//                gADCBuffer[ADC_BUFFER_WRAP(x-1)]/* next older sample */ > ADC_OFFSET)
-//            break;
-//    }
-//    // Step 3
-//    if (x == x_stop) // for loop ran to the end
-//        x = gADCBufferIndex - LCD_HORIZONTAL_MAX/2; // reset x back to how it was initialized
-//    return x;
-//}
+int FallingTrigger(void) // search for falling edge trigger
+{
+    // Step 1
+    int32_t locBufferIndex = gADCBufferIndex;
+    int x = locBufferIndex - LCD_HORIZONTAL_MAX/2/* half screen width; don’t use a magic number */;
+    // Step 2
+    int x_stop = x - ADC_BUFFER_SIZE/2;
+    for (; x > x_stop; x--) {
+        if (gADCBuffer[ADC_BUFFER_WRAP(x)] <= ADC_OFFSET &&
+                gADCBuffer[ADC_BUFFER_WRAP(x-1)]/* next older sample */ > ADC_OFFSET)
+            break;
+    }
+    // Step 3
+    if (x == x_stop) // for loop ran to the end
+        x = gADCBufferIndex - LCD_HORIZONTAL_MAX/2; // reset x back to how it was initialized
+    return x;
+}
 
 void loadBuffer(uint16_t* locBuffer, int trigger) {
     // Step 4
